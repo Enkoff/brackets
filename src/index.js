737a1faf76a3584d3c22d2b938931f5c84b092bf
Enkoff <0639818413@ukr.net>
2020-10-25 11:10:19 +0200

    fix: change check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var temp = str; 
+
+  if (str.length % 2 != 0) { 
+    return false;
+  } 
+
+  for (let i = 0; i < bracketsConfig.length; i++) { 
+      for (let j = 0; j < str.length; j++) { 
+          if (str[j] == bracketsConfig[i][0] && str[j+1] == bracketsConfig[i][1]) { 
+              temp = str.replace(bracketsConfig[i][0]+bracketsConfig[i][1], '') 
+          }
+      }
+  }
+  if (temp === str) {
+    return temp == '';
+  } else {
+    return check(temp, bracketsConfig);
+  }
 }
